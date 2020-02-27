f3021152582122086a1ab3b4263b3480a442b32e
victorreverter <victorreverter@gmail.com>
2020-02-27 04:36:09 +0100

    Updates

---
+++ precache-manifest.bfe0c33af3bb78e13be622bdb7e2b665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b417087a2ff64152dd9d58cb36ffd7b0",
+    "url": "/draw-app/index.html"
+  },
+  {
+    "revision": "51131f5b22181518a0ff",
+    "url": "/draw-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "caba6d6761b578a6ae07",
+    "url": "/draw-app/static/js/2.a4deadab.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/draw-app/static/js/2.a4deadab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51131f5b22181518a0ff",
+    "url": "/draw-app/static/js/main.3536dc47.chunk.js"
+  },
+  {
+    "revision": "f8d238398d2ba58f484d",
+    "url": "/draw-app/static/js/runtime-main.a508d52e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/draw-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
